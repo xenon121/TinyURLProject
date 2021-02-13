544b692b903f8fc6d57ee333cb0dde14ac28add6
Sandeep <sandeep@minebrat.com>
2021-02-13 13:15:25 +0530

    initial commit

---
+++ src/main/java/com/tinyurl/TinyUrlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tinyurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyUrlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyUrlProjectApplication.class, args);
+	}
+
+}
