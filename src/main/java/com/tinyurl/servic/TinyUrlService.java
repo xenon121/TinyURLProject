757535b823accc61f164becadba34ca71a3f3ba6
Sandeep <sandeep@minebrat.com>
2021-02-13 15:53:14 +0530

    redirect tiny link and get tiny url details by id both api implemented and tested.

--- src/main/java/com/tinyurl/servic/TinyUrlService.java
+++ src/main/java/com/tinyurl/servic/TinyUrlService.java
@@ -1,7 +1,16 @@
 package com.tinyurl.servic;
 
+import javax.servlet.http.HttpServletResponse;
+
+import com.tinyurl.exception.IdDoesNotExistsException;
+import com.tinyurl.exception.TinyUrlAlreadyExistException;
+
 public interface TinyUrlService {
 
-	Object createTinyUrl(String inputUrl) throws Exception;
+	Object createTinyUrl(String inputUrl) throws TinyUrlAlreadyExistException, Exception;
+
+	void redirectToFullUrl(String tinyUrl, HttpServletResponse httpServletResponse) throws IdDoesNotExistsException, Exception;
+
+	Object tinyUrlDetailsById(String tinyUrl) throws IdDoesNotExistsException, Exception;
 
 }
--- src/main/java/com/tinyurl/service/impl/TinyUrlServiceImpl.java
+++ src/main/java/com/tinyurl/service/impl/TinyUrlServiceImpl.java
@@ -1,9 +1,18 @@
 package com.tinyurl.service.impl;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.tinyurl.Response.CreateTinyUrlResponse;
 import com.tinyurl.entity.TinyUrlLinkDo;
+import com.tinyurl.exception.IdDoesNotExistsException;
 import com.tinyurl.exception.TinyUrlAlreadyExistException;
 import com.tinyurl.repository.TinyUrlRepository;
 import com.tinyurl.servic.TinyUrlService;
@@ -13,6 +22,9 @@ import net.bytebuddy.utility.RandomString;
 @Service
 public class TinyUrlServiceImpl implements TinyUrlService {
 
+	@Value(value = "${tiny.url}")
+	private String tinyUrl;
+	
 	@Autowired
 	private TinyUrlRepository tinyUrlRepository;
 	
@@ -31,8 +43,50 @@ public class TinyUrlServiceImpl implements TinyUrlService {
 		
 		tinyUrlLinkDo.setTinyUrlId(ranString);
 		tinyUrlLinkDo.setFullUrl(inputUrl);
+		tinyUrlLinkDo.setAccessCount(0);
 		
-		return tinyUrlRepository.save(tinyUrlLinkDo);
+		tinyUrlLinkDo = tinyUrlRepository.save(tinyUrlLinkDo);
+		
+		CreateTinyUrlResponse createTinyUrlResponse = new CreateTinyUrlResponse();
+		
+		createTinyUrlResponse.setTinyUrl(tinyUrl+"/"+tinyUrlLinkDo.getTinyUrlId());
+		
+		return createTinyUrlResponse;
+	}
+
+	@Override
+	public void redirectToFullUrl(String tinyUrl, HttpServletResponse httpServletResponse) throws IdDoesNotExistsException, Exception {
+		
+		Optional<TinyUrlLinkDo> tinyUrlLinkDo = tinyUrlRepository.findById(tinyUrl);
+		
+		if(!tinyUrlLinkDo.isPresent()) {
+			throw new IdDoesNotExistsException();
+		}
+		
+		if(tinyUrlLinkDo.get().getAccessCount() == null) {
+			tinyUrlLinkDo.get().setAccessCount(1);
+			
+		}else {
+			tinyUrlLinkDo.get().setAccessCount(tinyUrlLinkDo.get().getAccessCount() + 1);
+		}
+		
+		tinyUrlRepository.save(tinyUrlLinkDo.get());
+		
+		httpServletResponse.setHeader("Location", tinyUrlLinkDo.get().getFullUrl());
+		httpServletResponse.setStatus(302);
+		
+	}
+
+	@Override
+	public Object tinyUrlDetailsById(String tinyUrl) throws IdDoesNotExistsException, Exception {
+		
+		Optional<TinyUrlLinkDo> tinyUrlLinkDo = tinyUrlRepository.findById(tinyUrl);
+		
+		if(!tinyUrlLinkDo.isPresent()) {
+			throw new IdDoesNotExistsException();
+		}
+	
+		return tinyUrlLinkDo;
 	}
 
 }
